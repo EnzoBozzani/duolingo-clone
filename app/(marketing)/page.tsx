3da448af5e1d7efe78f9b1f46f5e29e1371db0e4
Enzo Bozzani <bozzanienzo@gmail.com>
2024-05-14 11:44:20 -0300

    feat: add authentication

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,20 +1,61 @@
 import { Button } from '@/components/ui/button';
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Home = () => {
 	return (
-		<div className='space-x-4'>
-			<Button>CLick me!</Button>
-			<Button variant={'primary'}>CLick me!</Button>
-			<Button variant={'primaryOutline'}>CLick me!</Button>
-			<Button variant={'secondary'}>CLick me!</Button>
-			<Button variant={'secondaryOutline'}>CLick me!</Button>
-			<Button variant={'danger'}>CLick me!</Button>
-			<Button variant={'dangerOutline'}>CLick me!</Button>
-			<Button variant={'super'}>CLick me!</Button>
-			<Button variant={'superOutline'}>CLick me!</Button>
-			<Button variant={'ghost'}>CLick me!</Button>
-			<Button variant={'sidebar'}>CLick me!</Button>
-			<Button variant={'sidebarOutline'}>CLick me!</Button>
+		<div className='max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2'>
+			<div className='relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0'>
+				<Image
+					src={'/hero.svg'}
+					fill
+					alt='hero'
+				/>
+			</div>
+			<div className='flex flex-col items-center gap-y-8 '>
+				<h1 className='text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center'>
+					Learn, practice, and master a new language with Lingo.
+				</h1>
+				<div>
+					<ClerkLoading>
+						<Loader className='w-5 h-5 text-muted-foreground animate-spin' />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<SignedOut>
+							<SignUpButton mode='modal'>
+								<Button
+									size={'lg'}
+									variant={'secondary'}
+									className='w-full'
+								>
+									Get Started
+								</Button>
+							</SignUpButton>
+							<SignInButton mode='modal'>
+								<Button
+									size={'lg'}
+									variant={'primaryOutline'}
+									className='w-full'
+								>
+									I already have an account
+								</Button>
+							</SignInButton>
+						</SignedOut>
+						<SignedIn>
+							<Button
+								size={'lg'}
+								variant={'secondary'}
+								asChild
+								className='w-full'
+							>
+								<Link href={'/learn'}>Continue learning</Link>
+							</Button>
+						</SignedIn>
+					</ClerkLoaded>
+				</div>
+			</div>
 		</div>
 	);
 };
