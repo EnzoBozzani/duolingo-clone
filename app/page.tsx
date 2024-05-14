b11c518334eec6052e92ccd0055fa85cb7f8d73e
Enzo Bozzani <bozzanienzo@gmail.com>
2024-05-14 10:47:23 -0300

    feat: create buttons library

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,19 @@ import { Button } from '@/components/ui/button';
 
 const Home = () => {
 	return (
-		<div>
-			<Button variant={'destructive'}>CLick me!</Button>
+		<div className='space-x-4'>
+			<Button>CLick me!</Button>
+			<Button variant={'primary'}>CLick me!</Button>
+			<Button variant={'primaryOutline'}>CLick me!</Button>
+			<Button variant={'secondary'}>CLick me!</Button>
+			<Button variant={'secondaryOutline'}>CLick me!</Button>
+			<Button variant={'danger'}>CLick me!</Button>
+			<Button variant={'dangerOutline'}>CLick me!</Button>
+			<Button variant={'super'}>CLick me!</Button>
+			<Button variant={'superOutline'}>CLick me!</Button>
+			<Button variant={'ghost'}>CLick me!</Button>
+			<Button variant={'sidebar'}>CLick me!</Button>
+			<Button variant={'sidebarOutline'}>CLick me!</Button>
 		</div>
 	);
 };
