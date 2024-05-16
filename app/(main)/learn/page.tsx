977e143f8d75181910a6039bad48538c81c2fe82
Enzo Bozzani <bozzanienzo@gmail.com>
2024-05-16 17:06:49 -0300

    feat: develop getUnits query

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-import { getUserProgress } from '@/db/queries';
+import { getUnits, getUserProgress } from '@/db/queries';
 import { FeedWrapper } from '@/components/feed-wrapper';
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { UserProgress } from '@/components/user-progress';
@@ -9,8 +9,9 @@ import { Header } from './header';
 
 const LearnPage = async () => {
 	const userProgressPromise = getUserProgress();
+	const unitsPromise = getUnits();
 
-	const [userProgress] = await Promise.all([userProgressPromise]);
+	const [userProgress, units] = await Promise.all([userProgressPromise, unitsPromise]);
 
 	if (!userProgress || !userProgress.activeCourse) {
 		redirect('/courses');
@@ -28,6 +29,14 @@ const LearnPage = async () => {
 			</StickyWrapper>
 			<FeedWrapper>
 				<Header title={userProgress.activeCourse.title} />
+				{/* {units.map((unit) => (
+					<div
+						key={unit.id}
+						className='mb-10'
+					>
+						{JSON.stringify(unit)}
+					</div>
+				))} */}
 			</FeedWrapper>
 		</div>
 	);
