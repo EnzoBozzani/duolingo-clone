7291f898e4cc295c53485174b692b2829e00e24b
Enzo Bozzani <bozzanienzo@gmail.com>
2024-06-28 14:58:45 -0300

    feat: lesson admin

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -6,9 +6,14 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import { CourseList } from './course/list';
 import { CourseCreate } from './course/create';
 import { CourseEdit } from './course/edit';
+
 import { UnitList } from './unit/list';
 import { UnitCreate } from './unit/create';
 import { UnitEdit } from './unit/edit';
+
+import { LessonList } from './lesson/list';
+import { LessonCreate } from './lesson/create';
+import { LessonEdit } from './lesson/edit';
 
 const dataProvider = simpleRestProvider('/api');
 
@@ -27,6 +32,13 @@ const App = () => {
 				list={UnitList}
 				create={UnitCreate}
 				edit={UnitEdit}
+				recordRepresentation='title'
+			/>
+			<Resource
+				name='lessons'
+				list={LessonList}
+				create={LessonCreate}
+				edit={LessonEdit}
 				recordRepresentation='title'
 			/>
 		</Admin>
